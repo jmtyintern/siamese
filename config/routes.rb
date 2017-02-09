87b8a04848029915dd8ff7d14c57d52fe0fa8cb8
kosuke.kato <kosuke.kato@jmpc-041.local>
2017-02-09 19:17:17 +0900

    data

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   resources :articles
-  devise_for :users
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+  }
+  get 'users/seccitons/show'
   get 'top/index'
 
   get 'top/show'
+  get 'article/index'
+  get 'article/show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'top#index'
