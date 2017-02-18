3e330c42c813fb2646bd7ba7d67c5097fd868762
hanatsuu <ryosuke.hanatsuka@jmty.jp>
2017-02-19 01:58:30 +0900

    ロケーションの追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
+  belongs_to :location
   validates :user_id, presence: true
   validates :title, presence: true
   validates :text, presence: true
