d74bb08bf89050c64daae323a5a575b9deb83af5
hanatsuu <ryosuke.hanatsuka@jmty.jp>
2017-02-25 16:24:50 +0900

    位置情報を付加して投稿する

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,11 +1,14 @@
 class Location < ApplicationRecord
   has_many :articles
 
-  before_save :search_lat_and_lon_from_geocoder
+  before_save :search_location_info_from_geocoder
 
+private
 
-
-  def search_lat_and_lon_from_geocoder
-    Geocoder.search("aa")
+  def search_location_info_from_geocoder
+    location_info  = Geocoder.search(name)[0]
+    self.address   = location_info.formatted_address
+    self.latitude  = location_info.geometry["location"]["lat"]
+    self.longitude = location_info.geometry["location"]["lng"]
   end
 end
