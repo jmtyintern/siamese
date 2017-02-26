a2729f77641d4835334087098cd7379d8a72d05b
kosuke.kato <kosuke.kato@jmpc-041.local>
2017-02-26 13:28:37 +0900

    articleインデックスのリスト整形した

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -32,7 +32,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process resize_to_fit: [50, 50]
+    process resize_to_fill(200, 200, gravity = ::Magick::CenterGravity)
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
